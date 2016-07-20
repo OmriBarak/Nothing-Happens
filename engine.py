bf206dca747552c79de65397a580f1a47111b463
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-20 05:32:33 -0400

    Added determine_valid_commands(), began working on it

--- engine.py
+++ engine.py
@@ -5,27 +5,7 @@ import json
 
 from config import config
 
-# Prepare game data and save file
-print "Loading game data..."
-with zipfile.ZipFile(config['gameFile'] + '.zip') as gameFile:
-    rooms = json.loads(gameFile.read(config['gameFile'] + '/rooms.json'))
-    items = json.loads(gameFile.read(config['gameFile'] + '/items.json'))
-    npcs = json.loads(gameFile.read(config['gameFile'] + '/npcs.json'))
-
-print "Checking for save file..."
-if not os.path.isfile(config['saveFile'] + '.zip'):
-    print "Save file not found. Creating new save file..."
-    # Save default state
-    with zipfile.ZipFile(config['gameFile'] + '.zip') as gameFile:
-        saveGame(
-            gameFile.read(config['gameFile'] + '/default-states/playerState.json'),
-            gameFile.read(config['gameFile'] + '/default-states/roomStates.json'),
-            gameFile.read(config['gameFile'] + '/default-states/itemStates.json'),
-            gameFile.read(config['gameFile'] + '/default-states/npcStates.json')
-        )
-else: print "Save file found."
-
-def loadGame():
+def load_game():
     with zipfile.ZipFile(config['saveFile'] + '.zip') as saveFile:
         playerState = saveFile.read('_SAVE/playerState.json')
         roomStates = saveFile.read('_SAVE/roomStates.json')
@@ -33,7 +13,7 @@ def loadGame():
         npcStates = saveFile.read('_SAVE/npcStates.json')
     return playerState, roomStates, itemStates, npcStates
 
-def saveGame(playerState, roomStates, itemStates, npcStates):
+def save_game(playerState, roomStates, itemStates, npcStates):
     print "Saving..."
     # Create a temporary save folder
     os.makedirs('_SAVE')
@@ -55,14 +35,60 @@ def saveGame(playerState, roomStates, itemStates, npcStates):
     shutil.rmtree('_SAVE')
     print "Saved."
 
-def playerTurn(command):
-    pass
+# Prepare game data and save file
+print "Loading game data..."
+with zipfile.ZipFile(config['gameFile'] + '.zip') as gameFile:
+    rooms = json.loads(gameFile.read(config['gameFile'] + '/rooms.json'))
+    items = json.loads(gameFile.read(config['gameFile'] + '/items.json'))
+    npcs = json.loads(gameFile.read(config['gameFile'] + '/npcs.json'))
 
-def engineTurn():
+print "Checking for save file..."
+if not os.path.isfile(config['saveFile'] + '.zip'):
+    print "Save file not found. Creating new save file..."
+    # Save default state
+    with zipfile.ZipFile(config['gameFile'] + '.zip') as gameFile:
+        save_game(
+            gameFile.read(config['gameFile'] + '/default-states/playerState.json'),
+            gameFile.read(config['gameFile'] + '/default-states/roomStates.json'),
+            gameFile.read(config['gameFile'] + '/default-states/itemStates.json'),
+            gameFile.read(config['gameFile'] + '/default-states/npcStates.json')
+        )
+else: print "Save file found."
+
+playerState, roomStates, itemStates, npcStates = load_game()
+
+def determine_valid_commands():
+    # Movement
+    goCommands = ["go", "move", "walk"]
+
+    nCommands = ["north", "n"]
+    sCommands = ["south", "s"]
+    eCommands = ["east", "e"]
+    wCommands = ["west", "w"]
+
+    neCommands = ["northeast", "ne"]
+    nwCommands = ["northwest", "nw"]
+    seCommands = ["southeast", "se"]
+    swCommands = ["southwest", "sw"]
+
+    upCommands = ["up", "u"]
+    downCommands = ["down", "d"]
+    inCommands = ["in"]
+    outCommands = ["out", "o"]
+
+    # todo: Special commands enabled by items
+
+    # Commands that are always valid
+    validCommands = ["look", "l", "inventory", "i", "take", "get"]
+
+def player_turn(command):
+    command = ' '.split(command.lower())
+
+def engine_turn():
     pass
 
 while True:
     # Main game loop
     command = raw_input(":: ")
-    playerTurn(command)
-    engineTurn()
+    player_turn(command)
+    engine_turn()
