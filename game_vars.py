1e9cf6c881c1cc338376ab22db93d703f520bcab
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-21 21:46:45 -0400

    Merged state() and thing() into fetch()

--- game_vars.py
+++ game_vars.py
@@ -15,22 +15,20 @@ def player_state():
     # Returns player state dict
     return playerState
 
-def state(thingType, thingID):
-    # Returns as dict the state of a thing (room, item, or NPC) of type and ID specified in args
-    if thingType == 'item': stateList = itemStates
-    elif thingType == 'npc': stateList = npcStates
-    elif thingType == 'room': stateList = roomStates
+def fetch(thingType, objectType, thingID):
+    # Returns as dict the state of or info for (as specified in args) a thing (room, item, or NPC) of type and ID specified in args
+    if thingType == 'state':
+        # Find states of things
+        if thingType == 'item': thingList = itemStates
+        elif thingType == 'npc': thingList = npcStates
+        elif thingType == 'room': thingList = roomStates
+
+    elif thingType == 'info':
+        # Find game info about things
+        if thingType == 'item': thingList = gameItems
+        elif thingType == 'npc': thingList = gameItems
+        elif thingType == 'room': thingList = gameRooms
 
     for thing in stateList:
-        if thing["ID"] == thingID:
-            return thing
-
-def thing(thingType, thingID):
-    # Returns as dict the game object of a thing (room, item, or NPC) of type and ID specified in args
-    if thingType == 'item': thingList = gameItems
-    elif thingType == 'npc': thingList = gameNpcs
-    elif thingType == 'room': thingList = gameRooms
-
-    for thing in thingList:
         if thing["ID"] == thingID:
             return thing
