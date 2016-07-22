6aae196cc8bbc4deb62954b59d34390f09dc253d
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-21 23:49:01 -0400

    Fixed bugs

--- room.py
+++ room.py
@@ -6,20 +6,35 @@ directionsLongcodes = {
     "u": "up", "d": "down", "i": "in", "o": "out"
 }
 
+def list_items(roomID):
+    # Returns (itemID, item name) for item in room
+    items = []
+    for itemID in infoutil.fetch('state', 'room', roomID)['items']: items.append((itemID, infoutil.name('item', itemID)))
+    return items
+
+def list_npcs(roomID):
+    # Returns (npcID, npc name) for npc in room
+    npcs = []
+    for npc in infoutil.npcStates:
+        if npc['location'] == roomID: npcs.append((npc['ID'], infoutil.name('npc', npc['ID'])))
+    return npcs
+
+def list_connections(roomID):
+    # Returns (roomID, room name, direction shortcode, direction longcode) for connections in room
+    connections = []
+    for connection in infoutil.fetch('info', 'room', roomID)['connections']: connections.append((connection['room'], (infoutil.name('room', connection['room']), connection['direction'], directionLongcodes[connection['direction']])))
+    return connections
+
 def describe(roomID):
     ## Create and return a description of the roomID in argument
-    # Get room information and state from the game's data files
-    roomInfo = infoutil.fetch('info', 'room', roomID)
-    roomState = infoutil.fetch('state', 'room', roomID)
-
     # Get the room's name and description
-    name =  infoutil.name('room', roomID)
-    description = room['description']
+    name = infoutil.name('room', roomID)
+    description = infoutil.fetch('info', 'room', roomID)['description']
 
     # Get items, NPCs, and connections for the room
-    items = items(roomID)
-    npcs = npcs(roomID)
-    connections = connections(roomID)
+    items = list_items(roomID)
+    npcs = list_npcs(roomID)
+    connections = list_connections(roomID)
 
     # Format the room information as a string
     itemSentence = u""
@@ -50,22 +65,3 @@ def describe(roomID):
     for sentence in passageList:
         if sentence != u"": passageStr += sentence[0].upper() + sentence[1:] + u" "
     return title + u"\n\n" + passageStr
-
-def items(roomID):
-    # Returns (itemID, item name) for item in room
-    items = []
-    for itemID in roomState['items']: items.append((itemID, infoutil.name('item', itemID)))
-    return items
-
-def npcs(roomID):
-    # Returns (npcID, npc name) for npc in room
-    npcs = []
-    for npc in infoutil.npcStates:
-        if npc['location'] == roomID: npcs.append((npc['ID'], infoutil.name('npc', npc['ID'])))
-    return npcs
-
-def connections(roomID):
-    # Returns (roomID, room name, direction shortcode, direction longcode) for connections in room
-    connections = []
-    for connection in roomInfo['connections']: connections.append((connection['room'], (infoutil.name('room', connection['room']), connection['direction'], directionLongcodes[connection['direction']])))
-    return connections
