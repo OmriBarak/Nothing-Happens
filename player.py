51e49aed80275979f9c968cf6afc21aba4e79819
Omri Barak <omribarak@pitt.edu>
2016-07-22 19:39:12 -0400

    Priming code for changes, asking a few questions

--- player.py
+++ player.py
@@ -16,15 +16,16 @@ movementIntents = movementCommands + directionCommands
 informationIntents = viewCommands + invCommands
 interactionIntents = invManagementCommands + usageCommands
 
-def list_inventory():
-    print infoutil.player_state()
-    return infoutil.player_state()['inventory']
-
 directionMap = {
     'n': ["north", "n"], 's': ["south", "s"], 'e': ["east", "e"], 'w': ["west", "w"],
     'ne': ["northeast", "ne"], 'nw': ["northwest", "nw"], 'se': ["southeast", "se"], 'sw': ["southwest", "sw"],
     'u': ["up", "u"], 'd': ["down", "d"], 'i': ["in"], 'o': ["out", "o"]
 }
+
+def list_inventory():
+    print infoutil.player_state()
+    return infoutil.player_state()['inventory']
+
 def do(command):
     ## Attempts to make the player character do the player's command
 
@@ -37,8 +38,12 @@ def do(command):
     if intent == "MOVE":
         # Find the direction part of the command
         direction = ""
+        
+        #TODO: Protect this from whether a player issues two directions in their command,
+        #aka "move northwest southwest"
         for word in command:
             if word in directionCommands: direction = word
+            #break ?
         
         # Get the direction's shortcode
         for shortcode in directionMap.keys():
@@ -49,16 +54,20 @@ def do(command):
         # Check to see if there is a room in that direction from the player's room
         destination = ""
         for connection in room.list_connections(infoutil.player_state()['location']):
+            #TODO: what
             if directionShortcode in connection[2]:
                 destination = connection[0]
                 direction = connection[3]
                 break
         
-        if destination == "": return "There is no room in that direction."       # Fail state
+        if destination == "":
+            return "There is no room in that direction."       # Fail state
+            #return narrator.noRoomInThatDirectionError <- consolidate error codes to one file for editability
         else:
             # Move the player
             infoutil.update_state('player', 'location', destination)
-            return "You move " + direction + "...\n" + room.describe(destination)
+            return "You move " + direction + "... \n" + room.describe(destination)
+            #return narrator.narrateMove(direction) + "\n" + room.describe(destination)
 
     elif intent == "INFO":
         # Catch inventory-related commands right off the bat (inventory, i, look inventory, l inv, etc.)
@@ -68,10 +77,12 @@ def do(command):
                 for itemID in list_inventory():
                     inventoryList.append(item.name(itemID)[0].upper() + item.name(itemID)[1:] + u"\n")
                 return u"You have:\n" + ''.join(inventoryList)
+                #return narrator.narrateItems(inventoryList)
 
         if command[0] in viewCommands:
             if len(command) == 1:       # Look room
                 return room.describe(infoutil.player_state()['location'])
+                #return narrator.describeRoom(infoutil.player_state()['location'])
             else:       # Look [object]
                 # todo: get list of objects in the room and carried by the player, return item description
                 pass
