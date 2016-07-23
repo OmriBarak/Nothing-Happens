4ae73267e870ca00633b59f71acaf011b2f028c1
Omri Barak <omribarak@pitt.edu>
2016-07-22 21:57:53 -0400

    Added describe function

--- item.py
+++ item.py
@@ -2,3 +2,6 @@ import infoutil
 
 def name(itemID):
     return infoutil.fetch('info', 'item', itemID)['name']
+    
+def describe(itemID):
+    return infoutil.fetch('info', 'item', itemID)['description']
