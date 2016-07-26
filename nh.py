588fb6456d1328d5355f37f650a8b9942c6ac89b
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-26 19:32:12 -0400

    We'll get to engine_turn() when we get to it

--- nh.py
+++ nh.py
@@ -5,10 +5,6 @@ import room
 import item
 import npc
 
-def engine_turn():
-    # Move npcs, increment timers
-    pass
-
 while True:
     ## Main game loop
     # Get the player's location
@@ -18,4 +14,3 @@ while True:
     # Get the player's command
     command = raw_input(infoutil.name('room', playerLocation).upper() + " :: ").lower().split(' ')
     print player.do(command)
-    engine_turn()
