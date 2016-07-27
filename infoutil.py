aab2bc4db0f0e0174e8351c63989b86005f8388f
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-26 20:07:15 -0400

    Added remove_item(), add_item(), improved fetch()

--- infoutil.py
+++ infoutil.py
@@ -7,18 +7,17 @@ import json
 import ioutil
 from config import filecfg
 
-def player_state():
-    # Returns player state
-    return playerState
-
-def fetch(dataType, objectType, thingID):
-    # Returns the state of or info for (as specified in args) a thing (room, item, or NPC) of type and ID specified in args
+def fetch(dataType, objectType, thingID=""):
+    ## Returns the state of or info for (as specified in args) a thing (room, item, or NPC) of type and ID specified in args
+    #  Note that 'player' objectTypes don't require a thingID
     thingList = []
     if dataType == 'state':
         # Find states of things
         if objectType == 'item': thingList = itemStates
         elif objectType == 'npc': thingList = npcStates
         elif objectType == 'room': thingList = roomStates
+
+        elif objectType == 'player': return playerState
 
     elif dataType == 'info':
         # Find game info about things
@@ -32,11 +31,20 @@ def fetch(dataType, objectType, thingID):
 
 def update_state(objectType, state, status, thingID=""):
     # Updates the state of the given object
+    # todo: Make this work without extra code for 'player' objectType
     if objectType == 'player': playerState[state] = status
     elif objectType == 'room': 
         for count, room in enumerate(roomStates):
             if room["ID"] == thingID: room[state] = status
-    # todo: update other object types
+
+def add_item():
+    # todo: write add_item()
+    pass
+
+def remove_item(holderObjectType, itemID, holderID=""):
+    # Removes item with ID itemID from its holder with ID holderID
+    heldItems = fetch('state', holderObjectType, holderID)['inventory']
+    update_state(holderObjectType, 'inventory', roomItems.remove(itemID), holderID)
 
 def name(objectType, thingID):
     # Returns as string the name of whatever the thingID specified in args
