71d846887fd4347b9f64f91c64829fbaa72a4676
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-26 20:21:04 -0400

    Fixed bug in nh

--- nh.py
+++ nh.py
@@ -7,10 +7,6 @@ import npc
 
 while True:
     ## Main game loop
-    # Get the player's location
-    playerState = infoutil.player_state()
-    playerLocation = playerState['location']
-
     # Get the player's command
-    command = raw_input("\n" + infoutil.name('room', playerLocation).upper() + " :: ").lower().split(' ')
+    command = raw_input("\n" + infoutil.name('room', infoutil.fetch('state', 'player', '0')['location']).upper() + " :: ").lower().split(' ')
     print player.do(command)
